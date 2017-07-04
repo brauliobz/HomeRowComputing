b5090ea82c1471deba6c67c772aee21cf90505cc
Bráulio Bezerra <brauliobezerra@gmail.com>
2017-07-04 16:55:52 -0300

    Switched to overlays so it know works on all programs.

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 DIFF=meld
 
-cp br_br /usr/share/X11/xkb/symbols/
+cp symbols/br_br      /usr/share/X11/xkb/symbols/
+cp symbols/us_ijkl    /usr/share/X11/xkb/symbols/
 
-$DIFF us /usr/share/X11/xkb/symbols/us
-$DIFF base.lst /usr/share/X11/xkb/rules/base.lst
-$DIFF base.xml /usr/share/X11/xkb/rules/base.xml
-$DIFF evdev.lst /usr/share/X11/xkb/rules/evdev.lst
-$DIFF evdev.xml /usr/share/X11/xkb/rules/evdev.xml
+$DIFF compat/accesx   /usr/share/X11/xkb/compat/accessx
+
+$DIFF rules/base.lst  /usr/share/X11/xkb/rules/base.lst
+$DIFF rules/base.xml  /usr/share/X11/xkb/rules/base.xml
+$DIFF rules/evdev.lst /usr/share/X11/xkb/rules/evdev.lst
+$DIFF rules/evdev.xml /usr/share/X11/xkb/rules/evdev.xml
 
